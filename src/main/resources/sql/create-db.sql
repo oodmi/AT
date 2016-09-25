6476aa9471a650aad74e6c72732c79761c254696
dmitry <dmitry.o.ovchinnikov@gmail.com>
2016-09-25 18:07:57 +0300

    get rid of excessive maven dependencies

---
+++ src/main/resources/sql/create-db.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS user CASCADE;
+DROP TABLE IF EXISTS book CASCADE;
+
+CREATE TABLE user (
+  login    VARCHAR(30) PRIMARY KEY NOT NULL,
+  password VARCHAR(50)             NOT NULL
+);
+
+CREATE TABLE book (
+  ISN    INT(11) PRIMARY KEY  NOT NULL,
+  author VARCHAR(30)          NOT NULL,
+  name   VARCHAR(30)          NOT NULL,
+  owner  VARCHAR(30)
+);
+
+ALTER TABLE book
+  ADD FOREIGN KEY (owner) REFERENCES user (login);
+
+
+
+
---
+++ src/main/resources/sql/insert-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user VALUES ('dmitry', 'lollol');
+INSERT INTO book VALUES (1, 'List A.' , 'Cats', 'dmitry');

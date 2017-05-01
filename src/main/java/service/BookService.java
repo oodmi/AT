4a01300bdc300248beeed82774a2c85a0a2d5d54
dmitry <dmitry.o.ovchinnikov@gmail.com>
2017-05-02 00:06:26 +0300

    add jndi lookup and transaction manager

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import persistence.Book;
 
 import java.util.List;
@@ -20,18 +21,22 @@ public class BookService {
     @Autowired
     UserService userService;
 
+    @Transactional
     public boolean insertBook(Book book) {
         return bookDao.insertBook(book);
     }
 
+    @Transactional
     public boolean updateBook(Book book) {
         return bookDao.updateBook(book);
     }
 
+    @Transactional
     public boolean deleteBook(Long isn) {
         return bookDao.deleteBook(isn);
     }
 
+    @Transactional
     public List<BookResponse> getBooks(Long offset, Long count) {
         List<Book> books = bookDao.getBooks(offset, count);
         return books.stream().map(
@@ -39,6 +44,7 @@ public class BookService {
         ).collect(Collectors.toList());
     }
 
+    @Transactional
     public void takeBook(Long isn) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String name = user.getUsername();
@@ -46,6 +52,7 @@ public class BookService {
         bookDao.takeBook(idByLogin.getId(), isn);
     }
 
+    @Transactional
     public void returnBook(Long isn) {
         bookDao.returnBook(isn);
     }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -4,6 +4,7 @@ import dao.UserDao;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import persistence.User;
 
 import java.util.List;
@@ -11,42 +12,49 @@ import java.util.List;
 @Service
 public class UserService {
 
-    private  static final Logger LOG = Logger.getLogger(UserService.class);
+    private static final Logger LOG = Logger.getLogger(UserService.class);
 
     @Autowired
     UserDao userDao;
 
+    @Transactional
     public boolean insertUser(User user) {
-        LOG.info("insert user {id:"  + user.getId() + ";login:" + user.getLogin()+";password:"+ user.getPassword()+"}");
+        LOG.info("insert user {id:" + user.getId() + ";login:" + user.getLogin() + ";password:" + user.getPassword() + "}");
         return userDao.insertUser(user);
     }
 
+    @Transactional
     public User getUserById(Long id) {
         LOG.info("get user by id: " + id);
         return userDao.getUserById(id);
     }
 
+    @Transactional
     public boolean updateUser(User user) {
-        LOG.info("update user {id:"  + user.getId() + ";login:" + user.getLogin()+";password:"+ user.getPassword()+"}");
+        LOG.info("update user {id:" + user.getId() + ";login:" + user.getLogin() + ";password:" + user.getPassword() + "}");
         return userDao.updateUser(user);
     }
 
+    @Transactional
     public boolean deleteUser(Long id) {
         LOG.info("delete user by id:" + id);
         return userDao.deleteUser(id);
     }
 
+    @Transactional
     public List<User> getUsers() {
         LOG.info("Get all Users");
         return userDao.getUsers();
     }
 
+    @Transactional
     public String getLoginById(Long id) {
         LOG.info("Get user login by id:" + id);
-        return userDao.getUserById(id)!=null?userDao.getUserById(id).getLogin():null;
+        return userDao.getUserById(id) != null ? userDao.getUserById(id).getLogin() : null;
     }
 
-    public User getIdByLogin(String login){
+    @Transactional
+    public User getIdByLogin(String login) {
         LOG.info("Get user id by login:" + login);
         return userDao.getIdByClass(login);
     }
